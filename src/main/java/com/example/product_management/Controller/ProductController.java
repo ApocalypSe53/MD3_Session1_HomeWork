65851b48700e7c6953f3848192b3f3ac9afefba4
ApocalypSe53 <vudinhanhvu1010@gmail.com>
2026-06-30 23:10:49 +0700

    Basic4 Commit

--- src/main/java/com/example/product_management/Controller/ProductController.java
+++ src/main/java/com/example/product_management/Controller/ProductController.java
@@ -1,7 +1,11 @@
 package com.example.product_management.Controller;
 
+import com.example.product_management.Model.Product;
 import com.example.product_management.Service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -9,4 +13,11 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @GetMapping("/api/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+
 }
